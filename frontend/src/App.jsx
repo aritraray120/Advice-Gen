d0aadf6d8f188ab7c1166a05d7353cd4a9500e00
Aritra Ray <aritraray120@gmail.com>
2025-03-25 19:18:12 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
 import { quotes } from "./assets/assets"; // Import the quotes
 
+let i = 0; // Declare `i` outside the function
+
 function App() {
+
   const [advice, setAdvice] = useState("");
 
   function handlerFunc() {
-    setAdvice(quotes[Math.floor(Math.random() * quotes.length)]);
+    setAdvice(quotes[i]);
+    i = (i + 1) % quotes.length; // Ensures `i` loops back to 0
   }
+
 
   return (
     <div className="flex h-screen items-center justify-center bg-[#1f2632]">
       <div className="flex w-[85vw] flex-col items-center justify-center rounded-2xl bg-[#323a49] px-[40px] pt-[20px] text-center sm:w-[540px]">
         <div className="my-[25px] text-base font-bold text-[#52ffa8]">ADVICE</div>
 
-      
+
         <div className="mb-[25px] min-h-[132px] sm:h-auto text-[28px] font-extrabold text-[#cee3e9]">
           {advice?.advice || "Click the dice for advice"}
         </div>
